ff80afff80d6e614361aaa2220adfb0738f735d6
WevertonNicolau <wevertonnicolau100@gmail.com>
2024-06-18 01:42:28 -0300

    add scan

--- DANF AT/lib/main.dart
+++ DANF AT/lib/main.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
@@ -177,50 +179,110 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('DANF - MQTT'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Text(
-              'Status: ${_connected ? 'Connected' : 'Disconnected'}',
-              style: TextStyle(fontSize: 20),
-            ),
-            TextField(
-              controller: _textController,
-              decoration: InputDecoration(
-                labelText: 'Message',
-              ),
-              onSubmitted: (text) {
-                if (_connected) {
-                  _publish(text);
-                }
-              },
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                if (_connected) {
-                  _publish(_textController.text);
-                }
-              },
-              child: Text('OK'),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Received: $_receivedMessage',
-              style: TextStyle(fontSize: 16),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('DANF - MQTT'),
         ),
-      ),
+        body: Padding(
+        padding: const EdgeInsets.all(16.0),
+    child: Column
+      (
+      children: <Widget>[
+        Text(
+          'Status: ${_connected ? 'Connected' : 'Disconnected'}',
+          style: TextStyle(fontSize: 20),
+        ),
+        TextField(
+          controller: _textController,
+          decoration: InputDecoration(
+            labelText: 'Message',
+          ),
+          onSubmitted: (text) {
+            if (_connected) {
+              _publish(text);
+            }
+          },
+        ),
+        SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {
+            if (_connected) {
+              _publish(_textController.text);
+            }
+          },
+          child: Text('OK'),
+        ),
+        SizedBox(height: 20),
+        Text(
+          'Received: $_receivedMessage',
+          style: TextStyle(fontSize: 16),
+        ),
+      ],
+    ),
+        ),
     );
   }
 }
 
-class IPPage extends StatelessWidget {
+class IPPage extends StatefulWidget {
+  @override
+  _IPPageState createState() => _IPPageState();
+}
+
+class _IPPageState extends State<IPPage> {
+  String _scanResult = '';
+
+  Future<void> _startScan() async {
+    // UDP socket for sending the broadcast message
+    RawDatagramSocket? udpSocket;
+
+    try {
+      udpSocket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
+      udpSocket.broadcastEnabled = true;
+
+      final String broadcastAddress = '255.255.255.255';
+      final int port = 5555;
+
+      // Send broadcast message
+      udpSocket.send(utf8.encode('<SI>'), InternetAddress(broadcastAddress), port);
+
+      // Listen for response for 2 seconds
+      await Future.delayed(Duration(seconds: 2));
+
+      // Receive response
+      await for (var datagram in udpSocket) {
+        if (datagram == RawSocketEvent.read) {
+          Datagram dg = udpSocket.receive()!;
+          String response = utf8.decode(dg.data);
+          List<String> info = extractInfo(response);
+          setState(() {
+            _scanResult = info.join('\n'); // Alteração aqui
+          });
+          break;
+        }
+      }
+    } catch (e) {
+      print('Error during scan: $e');
+    } finally {
+      udpSocket?.close();
+    }
+  }
+
+  List<String> extractInfo(String message) {
+    RegExp regex = RegExp(r"<([^>]+)><([^>]+)><([^>]+)><([^>]+)>");
+    Match? match = regex.firstMatch(message);
+
+    if (match != null) {
+      String name = match.group(1)!;
+      String ip = match.group(2)!;
+      String mac = match.group(3)!;
+      String version = match.group(4)!;
+      return ['Name: $name', 'IP: $ip', 'MAC: $mac', 'Version: $version'];
+    } else {
+      print("A mensagem não está no formato esperado.");
+      return [];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -228,9 +290,20 @@ class IPPage extends StatelessWidget {
         title: Text('IP Page'),
       ),
       body: Center(
-        child: Text(
-          'IP Page Content',
-          style: TextStyle(fontSize: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _startScan,
+              child: Text('Start Scan'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              _scanResult,
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.center, // Ajuste aqui
+            ),
+          ],
         ),
       ),
     );
@@ -253,3 +326,4 @@ class CenasPage extends StatelessWidget {
     );
   }
 }
+
